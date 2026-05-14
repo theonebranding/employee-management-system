f01a10c63ef666e651583a31678dfac018725ff1
rahil1202 <rahilisvahora@gmail.com>
2026-05-14 15:00:27 +0530

    style: font changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -41,7 +41,9 @@ export default {
       },
 
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+        sans: ['var(--font-primary)', 'var(--font-secondary)', 'var(--font-tertiary)', 'ui-sans-serif', 'system-ui'],
+        secondary: ['var(--font-secondary)', 'var(--font-tertiary)', 'ui-sans-serif', 'system-ui'],
+        tertiary: ['var(--font-tertiary)', 'ui-sans-serif', 'system-ui'],
         mono: ['Fira Code', 'ui-monospace', 'monospace'],
       },
 
